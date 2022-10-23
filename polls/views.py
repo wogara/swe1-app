58d296d69a2918fec7187eb2f16ffeed32791e66
wogara <weogara@gmail.com>
2022-10-23 14:53:43 -0400

    reformatted views to pass black

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponseRedirect
+
 # from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+
 # from django.template import loader
 from django.views import generic
 from .models import Choice, Question
